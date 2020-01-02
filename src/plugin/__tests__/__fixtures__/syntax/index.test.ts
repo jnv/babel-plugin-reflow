848cc5cb664955d591ed22515f57d8f09b82846d
Jan Vlnas <git@jan.vlnas.cz>
2020-01-02 15:19:19 +0100

    Add tests for optional chaining, nullsh coalescing

---
+++ src/plugin/__tests__/__fixtures__/syntax/index.test.ts
@@ -0,0 +1,11 @@
+import { resolve } from 'path';
+
+import { getTransformOptions } from '../../../util/options';
+import { runFixtureTests } from '../../runner';
+
+describe('Syntax', () => {
+  runFixtureTests(
+    resolve('src/plugin/__tests__/__fixtures__/syntax/'),
+    getTransformOptions(),
+  );
+});
---
+++ src/plugin/__tests__/__fixtures__/syntax/stage3/input.js
@@ -0,0 +1,24 @@
+// @flow
+type Customer = {
+  name: string,
+  details: {
+    age?: number,
+    location?: string,
+    address?: {
+      city: string,
+    }
+  }
+}
+
+let customer: Customer = {
+  name: 'Carl',
+  details: {
+    age: 82,
+    location: 'Paradise Falls',
+  }
+};
+// Optional chaining
+let customerCity = customer.details?.address?.city;
+
+// Nullish coalescing
+let customerAge = customer.details.age ?? 'Unknown age'
